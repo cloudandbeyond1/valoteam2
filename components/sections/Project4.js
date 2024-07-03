31211ff641c3a6433b41170411b0cbb77709ff6f
Cloud & beyond <cloudandbeyond2022@gmail.com>
2024-07-03 18:15:21 +0530

    intial

---
+++ components/sections/Project4.js
@@ -0,0 +1,30 @@
+
+import Link from 'next/link'
+import ProjectSlider1 from '../slider/ProjectSlider1'
+
+export default function Project4() {
+    return (
+        <>
+            <section className="project-section fix section-padding pt-0 mt-4" id="project">
+                <div className="container">
+                    <div className="section-title text-center">
+                        <span className="wow fadeInUp"> Our Projects</span>
+                        <h2 className="wow fadeInUp" data-wow-delay=".3s">
+                            Latest showcase and solutions <br /> to our customers
+                        </h2>
+                        <div className="header-button">
+                                        <Link href="/news" className="theme-btn" style={{padding:"12px"}}>
+                                            <span style={{color:"white",marginBottom:"0px"}}>
+                                            View More
+                                                <i className="fa-solid fa-arrow-right-long" />
+                                            </span>
+                                        </Link>
+                                    </div>
+                    </div>
+                </div>
+                <ProjectSlider1 />
+                
+            </section>
+        </>
+    )
+}
